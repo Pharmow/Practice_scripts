e2ecde309f2af2216985574c4e52813ea277a3cc
Pharmow <126022065+Pharmow@users.noreply.github.com>
2023-04-01 17:38:41 -0600

    Create love_score2.py

---
+++ love_score2.py
@@ -0,0 +1,32 @@
+print("Let's see how compartible you guys are: ")
+print(input("Please enter your name, follow by the name of your lover: "))
+love_score = random.randint(1, 100)
+if love_score >= 75 and love_score <= 100:
+  print(f"Your love score of {love_score} can melt the moon")
+elif love_score >= 50 and love_score <= 75:
+  print(f"Your love score of {love_score} is a very stong compartible score, I will love o ")
+elif love_score >= 25 and love_score <=50:
+  print(f"Your love score of {love_score} is manageable, but you must be fighting a lot ")
+else:
+  print(f"Your love score is {love_score}, there is fire on the mountain, run! run! run!")
+  print('''
+  ,////,
+                        /// 6|
+                        //  _|
+                       _/_,-'
+                  _.-/'/   \   ,/;,
+               ,-' /'  \_   \ / _/
+               `\ /     _/\  ` /
+                 |     /,  `\_/
+                 |     \'
+ pb  /\_        /`      /\
+   /' /_``--.__/\  `,. /  \
+  |_/`  `-._     `\/  `\   `.
+            `-.__/'     `\   |
+                          `\  \
+                            `\ \
+                              \_\__
+                               \___)
+  
+  
+  ''')
