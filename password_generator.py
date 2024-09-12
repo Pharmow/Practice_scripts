c5372ce8a19a08761600085bed0cbb95727e038f
Famo <femcast@yahoo.com>
2024-09-12 08:25:33 -0600

    updated password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -5,27 +5,26 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-
-n_letters = 8
-n_numbers = 5
-n_symbols = 3
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password = ""
-for letter in range(1, 9):
+for letter in range(0, nr_letters):
   password += random.choice(letters)
-for number in range(1, 6):
+for number in range(0, nr_numbers):
   password += random.choice(numbers)
-for symbol in range(1, 4):
+for symbol in range(0, nr_symbols):
   password += random.choice(symbols)
 print(password)
 
 
 password = []
-for letter in range(1, 9):
+for letter in range(0, nr_letters):
   password.append(random.choice(letters))
-for number in range(1, 6):
+for number in range(0, nr_numbers):
   password.append(random.choice(numbers))
-for symbol in range(1, 4):
+for symbol in range(0, nr_symbols):
   password.append(random.choice(symbols))
 # print(password)
 random.shuffle(password)
